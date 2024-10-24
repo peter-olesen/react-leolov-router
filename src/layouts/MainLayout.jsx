c9d35cbcc1eb20fb99f4a9c799194e648792e71a
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-10-24 13:56:57 +0200

    removed GA from index and added cookie component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 import { Navigation } from "../components/Navigation/Navigation"
 import { Footer } from "../components/Footer/Footer"
+import { CookieBanner } from "../components/CookieBanner/CookieBanner"
 
 export function MainLayout() {
+
     return (
         <>
+        <CookieBanner />
         <Navigation />
         <Outlet />
         <Footer />
